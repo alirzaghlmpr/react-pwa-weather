1a11f3defa4438c5ef6dc28c834529cda8805772
alirzaghlmpr <alirzaghlmpr@gmail.com>
2022-09-27 12:25:19 +0330

    finish developing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { fetchWeather } from "./Api";
+import "./Style";
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState(null);
+  const [isLoading, setLoader] = useState(false);
+
+  useEffect(() => {
+    "main" in weather ? setError(null) : setError(weather.message);
+  }, [weather]);
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      setLoader(true);
+      setWeather(await fetchWeather(query));
+      setQuery("");
+      setLoader(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <img src="./weather.png" alt="weather-icon" />
+      <h3 className="text-searching">Weather App</h3>
+
+      <input
+        type="text"
+        className="search"
+        placeholder="Search City ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {isLoading && (
+        <div>
+          <p className="text-searching"> Searching... </p>
+          <img src="./spinner.gif" alt="spinner" width={100} />
+        </div>
+      )}
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)} <sup>&deg;C</sup>{" "}
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p> {weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+      {error && (
+        <div className="city">
+          <h2 className="city-name">{error}</h2>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
